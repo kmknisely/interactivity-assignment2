f37f7718823564686027b093c6c59378ee713e70
Kenney Knisely <kknisely@mac.com>
2019-10-13 20:41:45 -0400

    Add footer and change blue colors to green

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p class="footer-text"> Created by: Kenney Knisely </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
   <?php wp_footer();?>
   </body>
 </html>
