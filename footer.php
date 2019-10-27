f006cafa414e765bc6a7a4401c3fc0373e91058f
Kenney Knisely <kknisely@mac.com>
2019-10-26 20:51:13 -0400

    Style widget option in footer

--- footer.php
+++ footer.php
@@ -2,17 +2,17 @@
   <div class="container">
       <div class="row">
           <!-- Left -->
-          <div class="col-md-4">
+          <div class="col-md-4 d-flex align-items-center">
             <?php dynamic_sidebar('footer-left'); ?>
           </div>
 
           <!-- Middle -->
-          <div class="col-md-4">
+          <div class="col-md-4 d-flex align-items-center">
             <?php dynamic_sidebar('footer-middle'); ?>
           </div>
 
           <!-- Right -->
-          <div class="col-md-4">
+          <div class="col-md-4 d-flex align-items-center">
             <?php dynamic_sidebar('footer-right'); ?>
           </div>
       </div>
